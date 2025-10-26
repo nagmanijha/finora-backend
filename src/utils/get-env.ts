48569a104cddb678e73226507716e595b7ac329b
Nagmani Jha <nagmanijha31@gmail.com>
2025-10-26 16:00:00 +0530

    setup

---
+++ src/utils/get-env.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+export const getEnv = (key: string, defaultValue?: string): string => {
+  const value = process.env[key];
+  if (value === undefined) {
+    if (defaultValue === undefined) {
+      throw new Error(`Environment variable ${key} is not set`);
+    }
+    return defaultValue;
+  }
+  return value;
+};
